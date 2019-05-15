037e50238a6a5e2554841a76b5050a591a514e86
Inyong Jung <jiy8319@gmail.com>
2019-05-15 13:02:36 +0900

    setting init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, Button} from 'reactstrap';
+
+function App() {
+  return (
+    <Container>
+      <Button style={{width:'100%'}}>11</Button>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/store/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Container, Button} from 'reactstrap';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Main;
