9954a9c6a7c8e0a87cf1ece6b2a84338a2d353a3
Inyong Jung <jiy8319@gmail.com>
2019-05-15 20:29:12 +0900

    head component set

--- src/components/user/menuListView/IndexView.js
+++ src/components/user/menuListView/IndexView.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
-import { Container, Button } from "reactstrap";
+import { Input, Button } from "reactstrap";
+import "../../../styles/menuListView.css";
 
 export default class IndexView extends Component {
   render() {
     return (
       <div>
-        <div>11</div>
-        11
+        <div className="IndexView-head">
+          <Input
+            className="IndexView InputSearch"
+            type="search"
+            placeholder="search placeholder"
+          />
+          <Button outline color="primary" className="IndexView ButtonSearch">
+            검
+          </Button>
+          <Button outline color="primary" className="IndexView ButtonPocket">
+            장
+          </Button>
+        </div>
       </div>
     );
   }
