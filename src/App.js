d3312021f32a10df5b150b1844beb834a360ee59
Inyong Jung <jiy8319@gmail.com>
2019-05-17 16:33:22 +0900

    add App Router MenuDetailView

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
-import MenuListView from "./pages/MenuListView";
 import StoreMainView from "./components/store/storeMainView/Main";
 import LoginView from "./pages/LoginView";
 import Test from "./pages/Test";
 import SignUp from "./pages/SignUp";
+import MenuListView from "./pages/MenuListView";
+import MenuDetailView from "./pages/MenuDetailView";
 import { Container } from "reactstrap";
 import "./styles/app.css";
 
@@ -15,7 +16,9 @@ function App() {
         <Route exact path="/" component={LoginView} />
         <Route path="/about" component={Test} />
         <Route path="/signUp" component={SignUp} />
-        <Route path="/store" component={StoreMainView} />   
+        <Route path="/menuList" component={MenuListView} />
+        <Route path="/menuDetail" component={MenuDetailView} />
+        <Route path="/store" component={StoreMainView} />
       </BrowserRouter>
     </Container>
   );
