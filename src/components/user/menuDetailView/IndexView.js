5e73d57e2b9ad2c896802bcc22a1d2a02389ae9b
Inyong Jung <jiy8319@gmail.com>
2019-05-17 23:11:24 +0900

    done menuDetailView form, not CSS

--- src/components/user/menuDetailView/IndexView.js
+++ src/components/user/menuDetailView/IndexView.js
@@ -2,8 +2,16 @@ import React, { Component } from "react";
 import { Row, Col, Button } from "reactstrap";
 import "../../../styles/menuListView.css";
 import img from "../../../image/img_1.png";
+import SelectOptional from "./SelectOptional";
 
 export default class IndexView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: 1
+    };
+  }
   render() {
     const menuImageSize = 200;
     return (
@@ -20,18 +28,41 @@ export default class IndexView extends Component {
           <div className="MenuDetail-select">
             <Row>
               <Col>
-                <div className="MenuDetail-selection">ice/hot</div>
+                <div className="MenuDetail-selection">
+                  <SelectOptional data={["ICE", "HOT"]} />
+                </div>
               </Col>
               <Col>
-                <div className="MenuDetail-selection">ice/hot</div>
+                <div className="MenuDetail-selection">
+                  <SelectOptional data={["S", "M", "L"]} />
+                </div>
               </Col>
               <Col>
-                <div className="MenuDetail-selection">ice/hot</div>
+                <div className="MenuDetail-selection">
+                  <input type="number" />
+                </div>
               </Col>
             </Row>
           </div>
         </div>
-        <div className="MenuDetail-foot">footer</div>
+        <div className="MenuDetail-foot">
+          <Row>
+            <Col sm="12" md={{ size: 6, offset: 5 }}>
+              <div style={{ display: "flex", margin: "30px" }}>
+                <div style={{ padding: "30px" }}>
+                  <Button outline color="primary">
+                    확인
+                  </Button>
+                </div>
+                <div style={{ padding: "30px" }}>
+                  <Button outline color="primary">
+                    취소
+                  </Button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
