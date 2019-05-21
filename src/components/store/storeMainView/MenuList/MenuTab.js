fb76851f925304536c0776ba1259658184163733
kimjounguk <dnrl24a@gmail.com>
2019-05-21 19:08:53 +0900

    store/inventory

---
+++ src/components/store/storeMainView/MenuList/MenuTab.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import MenuList from "./MenuList";
+import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
+import "../../../../styles/MenuTab.css";
+
+export default class MenuTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "1"
+    };
+  }
+  _toggle = tab => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  };
+
+  render() {
+    const { tapList, menus, clickTap } = this.props;
+    return (
+      <div id="MenuTab">
+        <Nav tabs>
+          {tapList.map((category, index) => (
+            <NavItem
+              key={index}
+              onClick={() => {
+                clickTap(category);
+              }}
+            >
+              <NavLink
+                onClick={() => {
+                  this._toggle(index + 1 + "");
+                }}
+              >
+                {category}
+              </NavLink>
+            </NavItem>
+          ))}
+        </Nav>
+        <TabContent
+          activeTab={this.state.activeTab}
+          style={{ marginTop: "20px", border: "5px" }}
+        >
+          <TabPane tabId={"1"}>
+            {menus.map((menu, index) => (
+              <MenuList selectMenu={menu} key={index} />
+            ))}
+          </TabPane>
+          <TabPane tabId={"2"}>
+            {menus.map((menu, index) => (
+              <MenuList selectMenu={menu} key={index} />
+            ))}
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
+}
---
+++ src/pages/StoreView.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Orders from "../components/store/storeMainView/OrderList/Orders";
+import StoreMain from "../components/store/storeMainView/StoreMain";
+import MenuCardTable from "../components/store/storeMainView/MenuList/MenuCardTable";
+import "../styles/StoreView.css";
+import { Tab, Row, Col, Nav } from "react-bootstrap";
+
+export class StoreView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      storeId: "1"
+    };
+  }
+
+  render() {
+    return (
+      <div id="store-body">
+        <Tab.Container id="left-tabs-example" defaultActiveKey="init">
+          <Row>
+            <Col id="storeView-col" sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item className="left-tap">
+                  <Nav.Link eventKey="first" onClick={this.onClick}>
+                    주문 현황
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="left-tap">
+                  <Nav.Link eventKey="second" onClick={this.onClick}>
+                    메뉴 관리
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="init">
+                  <StoreMain />
+                </Tab.Pane>
+                <Tab.Pane eventKey="first">
+                  <Orders />
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <MenuCardTable />
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </div>
+    );
+  }
+}
+
+export default StoreView;
